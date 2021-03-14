c8d138ac5c3be4a10982cc8d31093b9678e41a9d
carasel <oddsteph@gmail.com>
2021-03-14 21:44:39 +0000

    extract table rows

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import requests
+
 from bs4 import BeautifulSoup
 
 
-url = 'https://groups.freecycle.org/group/CardiffUK/posts/all?page=2&resultsperpage=100&showall=on&include_offers=off&include_wanteds=off&include_receiveds=off&include_takens=off'
+url = 'https://groups.freecycle.org/group/CardiffUK/posts/all?page=2&resultsperpage=100&showall=on'
 page = requests.get(url)
 
 soup = BeautifulSoup(page.content, 'html.parser')
+
+table = soup.find(id='group_posts_table')
+table_rows = table.find_all('tr')
+
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text for i in td]
+    print(row)
